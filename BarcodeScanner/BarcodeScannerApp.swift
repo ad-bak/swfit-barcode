c2593c01c353db4249cbec56eeeff8343a35fab0
adbak <adbak.dev@gmail.com>
2025-04-15 23:36:27 +0200

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Adnan Bakaev  on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
