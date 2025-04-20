ac9f9e1de3f28a699a624f4ff18616e196dd0b55
adbak <adbak.dev@gmail.com>
2025-04-20 23:19:42 +0200

    Add comprehensive .gitignore file

---
+++ BarcodeScanner/ScannerVC.swift
@@ -0,0 +1,8 @@
+//
+//  ScannerVC.swift
+//  BarcodeScanner
+//
+//  Created by Adnan Bakaev  on 19/04/2025.
+//
+
+import Foundation
---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Adnan Bakaev  on 20/04/2025.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
